3e0f1e1f0852e0272a2c781e4ca7024171e83158
Michael Benzinger <michaelbenzinger3@gmail.com>
2021-07-12 18:14:03 -0500

    Tasks can be added, completed, and deleted

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import './meyerreset.css';
 import './style.css';
 import { format } from 'date-fns';
 
-const todoFactory = (name, notes, dueDate, list, duration, priority) => {
+const todoFactory = (name, notes, dueDate, list, duration, priority, id) => {
   const startDate = new Date(Date.now());
-  const completed = false;
-  return {name, notes, dueDate, list, duration, priority, startDate, completed};
+  let completed = false;
+  return {name, notes, dueDate, list, duration, priority, id, startDate, completed};
 };
 
 const listFactory = (name, description) => {
@@ -13,16 +13,50 @@ const listFactory = (name, description) => {
   const addTodo = (task) => {
     todos.push(task);
   }
-  return {name, description, todos, addTodo};
+  const removeTodo = id => {
+    const index = getIndexFromId(id);
+    todos.splice(index, 1);
+  }
+  const toggleCompleted = id => {
+    const index = getIndexFromId(id);
+    if (todos[index].completed == false) {
+      todos[index].completed = true;
+      return true;
+    } else {
+      todos[index].completed = false;
+      return false;
+    }
+  }
+  const getIndexFromId = id => {
+    const index = todos.findIndex(todo => {
+      if (todo.id == id) {
+        return true;
+      }
+    });
+    return index;
+  }
+  return {name, description, todos, addTodo, removeTodo, toggleCompleted};
 }
 
 const taskHandler = (() => {
-  const addTask = ({name, notes, dueDate, list, duration, priority}) => {
-    const newTask = todoFactory(name, notes, dueDate, list, duration, priority);
-    database.getCurrentList().addTodo(newTask);
-  };
+  let id = 0;
+  const addTask = ({name, notes, dueDate, list, duration, priority}, currentList) => {
+    const newTask = todoFactory(name, notes, dueDate, list, duration, priority, id);
+    currentList.addTodo(newTask);
+    id ++;
+  }
+  const removeTask = (id, list) => {
+    list.removeTodo(id);
+    displayHandler.clearList();
+    displayHandler.displayList(list);
+  }
+  const getId = () => {
+    return id;
+  }
   return {
     addTask,
+    removeTask,
+    getId
   };
 })();
 
@@ -31,84 +65,111 @@ const displayHandler = (() => {
   addButton.addEventListener('click', () => {
     makeAddArea();
   });
+  document.addEventListener('keydown', function(e) {
+    if (e.shiftKey && e.key == 'N') {
+      if (document.querySelector('.add-area') == null) {
+        e.preventDefault();
+        makeAddArea();
+      }
+    }
+  });
 
   const makeAddArea = () => {
-    const addArea = document.createElement('div');
-    addArea.classList.add('add-area');
+    console.log(document.querySelector('.add-area'));
+    if (document.querySelector('.add-area') != null) {
+      return false;
+    } else {
+      const addArea = document.createElement('div');
+      addArea.classList.add('add-area');
 
-    const addDialog = document.createElement('div');
-    addDialog.classList.add('add-dialog');
+      const addDialog = document.createElement('div');
+      addDialog.classList.add('add-dialog');
 
-    const aDName = document.createElement('input');
-    aDName.classList.add('a-d-name');
-    aDName.autocomplete = 'chrome-off';
-    aDName.placeholder = 'Name';
+      const aDName = document.createElement('input');
+      aDName.classList.add('a-d-name');
+      aDName.autocomplete = 'chrome-off';
+      aDName.placeholder = 'Name';
 
-    const aDNotes = document.createElement('textarea');
-    aDNotes.classList.add('a-d-notes');
-    aDNotes.placeholder = 'Notes'
-    aDNotes.maxLength = '200';
+      const aDNotes = document.createElement('textarea');
+      aDNotes.classList.add('a-d-notes');
+      aDNotes.placeholder = 'Notes'
+      aDNotes.maxLength = '200';
 
-    const aDFooter = document.createElement('div');
-    aDFooter.classList.add('a-d-footer');
+      const aDFooter = document.createElement('div');
+      aDFooter.classList.add('a-d-footer');
 
-    const aDDate = document.createElement('input');
-    aDDate.classList.add('a-d-date');
-    aDDate.type = 'date';
-    
-    const aDList = document.createElement('input');
-    aDList.classList.add('a-d-list');
-    aDList.placeholder = 'list';
+      const aDDate = document.createElement('input');
+      aDDate.classList.add('a-d-date');
+      aDDate.type = 'date';
+      
+      const aDList = document.createElement('input');
+      aDList.classList.add('a-d-list');
+      aDList.placeholder = 'list';
 
-    const aDPriority = document.createElement('input');
-    aDPriority.classList.add('a-d-priority');
-    aDPriority.placeholder = 'priority';
+      const aDPriority = document.createElement('input');
+      aDPriority.classList.add('a-d-priority');
+      aDPriority.placeholder = 'priority';
 
-    const aDDuration = document.createElement('input');
-    aDDuration.classList.add('a-d-duration');
-    aDDuration.placeholder = 'duration';
+      const aDDuration = document.createElement('input');
+      aDDuration.classList.add('a-d-duration');
+      aDDuration.placeholder = 'duration';
 
-    const addTaskBtn = document.createElement('button');
-    addTaskBtn.classList.add('add-task-btn');
-    addTaskBtn.innerText = 'Add task';
-    addTaskBtn.addEventListener('click', () => {
-      // console.log(getTaskFromInput());
-      taskHandler.addTask(exportTaskInput());
-      clearList();
-      displayList(database.getCurrentList());
-      removeAddArea();
-    });
+      const addTaskBtn = document.createElement('button');
+      addTaskBtn.classList.add('add-task-btn');
+      addTaskBtn.innerText = 'Add task';
+      addTaskBtn.addEventListener('click', () => {
+        // console.log(getTaskFromInput());
+        addInputtedTask();
+      });
 
-    const cancelTaskBtn = document.createElement('button');
-    cancelTaskBtn.classList.add('cancel-task-btn');
-    cancelTaskBtn.innerText = 'Cancel';
-    cancelTaskBtn.addEventListener('click', () => {
-      removeAddArea();
-    });
+      const cancelTaskBtn = document.createElement('button');
+      cancelTaskBtn.classList.add('cancel-task-btn');
+      cancelTaskBtn.innerText = 'Cancel';
+      cancelTaskBtn.addEventListener('click', () => {
+        removeAddArea();
+      });
 
-    aDFooter.appendChild(aDDate);
-    aDFooter.appendChild(aDList);
-    aDFooter.appendChild(aDPriority);
-    aDFooter.appendChild(aDDuration);
-    addDialog.appendChild(aDName);
-    addDialog.appendChild(aDNotes);
-    addDialog.appendChild(aDFooter);
-    addArea.appendChild(addDialog);
-    addArea.appendChild(addTaskBtn);
-    addArea.appendChild(cancelTaskBtn);
+      aDFooter.appendChild(aDDate);
+      aDFooter.appendChild(aDList);
+      aDFooter.appendChild(aDPriority);
+      aDFooter.appendChild(aDDuration);
+      addDialog.appendChild(aDName);
+      addDialog.appendChild(aDNotes);
+      addDialog.appendChild(aDFooter);
+      addArea.appendChild(addDialog);
+      addArea.appendChild(addTaskBtn);
+      addArea.appendChild(cancelTaskBtn);
 
-    document.querySelector('.list-content-main').appendChild(addArea);
+      addArea.addEventListener('keydown', function(e) {
+        if (e.key == 'Enter') {
+          addInputtedTask();
+        } else if (e.key == 'Escape') {
+          removeAddArea();
+        }
+      });
 
-    aDName.focus();
+      document.querySelector('.list-content-main').appendChild(addArea);
 
-    const exportTaskInput = () => {
-      const name = aDName.value;
-      const notes = aDNotes.value;
-      const dueDate = aDDate.value;
-      const list = aDList.value;
-      const priority = aDPriority.value;
-      const duration = aDDuration.value;
-      return {name, notes, dueDate, list, duration, priority};
+      aDName.focus();
+
+      const addInputtedTask = () => {
+        if (aDName.value != "") {
+          taskHandler.addTask(exportTaskInput(), database.getCurrentList());
+          clearList();
+          displayList(database.getCurrentList());
+          removeAddArea();
+        }
+      }
+      const exportTaskInput = () => {
+        const name = aDName.value;
+        const notes = aDNotes.value;
+        const dueDate = aDDate.value;
+        const list = aDList.value;
+        const priority = aDPriority.value;
+        const duration = aDDuration.value;
+        return {name, notes, dueDate, list, duration, priority};
+      }
+      return true;
     }
   }
   const removeAddArea = () => {
@@ -132,20 +193,37 @@ const displayHandler = (() => {
     const listItems = document.querySelector('.list-items');
     const listItem = document.createElement('div');
     listItem.classList.add('list-item');
+    listItem.dataset.id = task.id;
     const itemBox = document.createElement('div');
     itemBox.classList.add('list-item-box');
-    const itemName = document.createElement('span');
+    const itemName = document.createElement('div');
+    itemName.classList.add('item-name');
     itemName.innerText = task.name;
     listItem.appendChild(itemBox);
     listItem.appendChild(itemName);
     listItems.appendChild(listItem);
 
+    if (task.completed == true) {
+      listItem.classList.add('list-item-completed');
+    }
+
     listItem.addEventListener('contextmenu', function(e) {
       displayItemContextMenu(e);
       e.preventDefault();
     }, false);
+    itemBox.addEventListener('click', function(e) {
+      if (database.getCurrentList().toggleCompleted(e.target.parentElement.dataset.id)) {
+        itemBox.parentElement.classList.add('list-item-completed');
+      } else {
+        itemBox.parentElement.classList.remove('list-item-completed');
+      }
+    });
   }
   const displayItemContextMenu = (event) => {
+    const existingContextMenu = document.querySelector('.context-menu');
+    if (existingContextMenu != null) {
+      existingContextMenu.remove();
+    }
     // Select the list-item element, not the checkbox or the task name span
     let target = event.target;
     if (target.classList[0] != 'list-item') target = target.parentElement;
@@ -157,7 +235,8 @@ const displayHandler = (() => {
     contextMenuDelete.innerText = 'Delete';
 
     contextMenuDelete.addEventListener('click', function(e) {
-      console.log('You are deleting ' + target.childNodes[1].innerText);
+      console.log('You are deleting ' + target.dataset.id);
+      taskHandler.removeTask(target.dataset.id, database.getCurrentList());
       contextMenu.remove();
     });
 
@@ -170,6 +249,12 @@ const displayHandler = (() => {
     
     contextMenu.style.top = `${y}px`;
     contextMenu.style.left = `${x}px`;
+
+    document.addEventListener('click', function(e) {
+      if (e.target != contextMenu) {
+        contextMenu.remove();
+      }
+    });
 
     // contextMenu.style.bottom = `${-y + contextMenu.offsetHeight/4}px`;
     // contextMenu.style.left = `${x - contextMenu.offsetWidth/2}px`;
@@ -184,10 +269,10 @@ const displayHandler = (() => {
 
 const database = (() => {
   let currentList = listFactory('Inbox', 'Description of inbox');
-  currentList.addTodo(todoFactory(`Book flights`));
-  currentList.addTodo(todoFactory(`Read about the metro`));
-  currentList.addTodo(todoFactory(`Borrow Sarah's travel guide`));
-  currentList.addTodo(todoFactory(`Book a hotel room`));
+  taskHandler.addTask({'name': `Book flights`}, currentList);
+  taskHandler.addTask({'name': `Read about the metro`}, currentList);
+  taskHandler.addTask({'name': `Borrow Sarah's travel guide`}, currentList);
+  taskHandler.addTask({'name': `Book a hotel room`}, currentList);
   const getCurrentList = () => {
     return currentList;
   }
